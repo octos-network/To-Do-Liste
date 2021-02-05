de2dcca42f9ed5f7d474dd1422fd081cfef3bf66
octos-network <octochicken7aber3@gmail.com>
2021-02-05 08:53:17 +0100

    More Humantal

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -3,6 +3,8 @@ package com.example.android.architecture.blueprints.todoapp.statistics
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import junit.framework.Assert.assertEquals
 import junit.framework.TestCase
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 class StatisticsUtilsTest {
@@ -10,20 +12,22 @@ class StatisticsUtilsTest {
     @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
 
-        // Create an active tasks (the false makes this active)
+        // GIVEN a list of tasks with a single, active, tasks
         val tasks = listOf<Task>(
                 Task("title", "desc", isCompleted = false)
         )
-        // Call our function
+        // WHEN you call getActiveAndCompleteStats
         val result = getActiveAndCompletedStats(tasks)
 
-        // Check the result
-        assertEquals(result.completedTasksPercent, 0f)
-        assertEquals(result.activeTasksPercent, 100f)
+        // THEN there are 0% completed tasks and 100% active tasks
+        assertThat(result.completedTasksPercent, `is` (0f))
+        assertThat(result.activeTasksPercent, `is` (100f))
     }
 
     @Test
     fun getActiveAndCompletedStats_both_returnsFortySixty() {
+
+        // Create an active tasks (the false makes this active)
         val tasks = listOf<Task>(
                 Task("title", "desc", isCompleted = true),
                 Task("title", "desc", isCompleted = true),
@@ -32,8 +36,10 @@ class StatisticsUtilsTest {
                 Task("title", "desc", isCompleted = false)
         )
 
+        // Call our function
         val result = getActiveAndCompletedStats(tasks)
 
+        // Check the result
         assertEquals(result.completedTasksPercent, 40f)
         assertEquals(result.activeTasksPercent, 60f)
     }
